1be9a09023b3508530ba1784697d7c916487dd4a
yeraycaballero <yeraycaballero@gmail.com>
2013-04-07 22:16:56 +0100

    rename fsFinder to fsquery

---
+++ src/fsquery.js
@@ -0,0 +1,48 @@
+
+var _       = require('lodash'),     
+    fs      = require('fs'),
+    util    = require('util'),
+    path    = require('path'),
+    events  = require('events'),
+    Spec    = require('./spec').Spec;
+
+
+function FSQuery(sPath) {
+    this.queryPath = _setPath.call(this, sPath);
+};
+
+util.inherits(FSQuery, events.EventEmitter);
+
+FSQuery.prototype.where = function(config) {
+  fs.readdir(this.queryPath, function(err, files) {
+    if (err) this.emit('error', err);
+    _findFiles.apply(this, [files, config]);  
+  }.bind(this));
+}
+
+var _setPath = function(sPath) {
+  return (sPath[sPath.length - 1] != path.sep)? sPath + path.sep : sPath; 
+}
+
+var _findFiles = function(files, config) {
+  var count = files.length;
+  var foundFilesCount = 0;
+
+  var onFile = function(file) {
+    foundFilesCount++;   
+    this.emit('file', file);
+  }.bind(this);
+
+  var onDone = function(count) {
+    this.emit('done', foundFilesCount);
+  }.bind(this);
+
+  _.each(files, function(file) {
+    var spec = new Spec(config);
+    spec.satisfies(this.queryPath + file, onFile);  
+    if (--count == 0) onDone();
+  }.bind(this))
+
+};
+
+exports.FSQuery = FSQuery;
---
+++ test/fsquery.test.js
@@ -0,0 +1,59 @@
+var fs     = require('fs'),
+    path   = require('path'),
+    should = require('should'),
+    gt     = require('../src/operators').gt,
+    FSQuery = require('../src/fsquery').FSQuery;
+
+
+describe('fsQuery', function() {
+    var fsQuery = null;
+
+    beforeEach(function() {
+    	fsQuery = new FSQuery('./assets');
+    })
+
+	it ('should find a file by filename', function(done) {		
+      fsQuery.where({ filename : 'jordan'});
+
+      fsQuery.on('file', function(file) {
+      	var filename = path.basename(file).replace(path.extname(file), '');
+	    if (filename.should.equal('jordan')) done();
+      })
+	});
+
+	it ('should find a file by extension', function(done) {
+	  fsQuery.where({ ext: '.png'});
+
+	  fsQuery.on('file', function(file) {
+	    if (path.extname(file).should.equal('.png')) done();
+	  });
+	});
+
+  it ('should find a file by size', function(done) {
+  	fsQuery.where({ size : 14730});
+
+  	fsQuery.on('file', function(file) {
+      var size = fs.statSync(file).size;
+      if (size.should.equal(14730)) done();
+  	});
+  });
+
+  it ('should find a file with size greater than 20000', function(done) {
+  	fsQuery.where({ size : gt(20000)});
+
+  	fsQuery.on('file', function(file) {
+  		var size = fs.statSync(file).size;
+  		if (size.should.greaterThan(20000)) done();
+  	})
+  });
+
+  it ('should find a file by multiple a composite spec', function(done) {
+    fsQuery.where({ filename : 'jordan', ext : '.png'});
+
+    fsQuery.on('file', function(file) {
+      if ( (path.basename(file).should.equal('jordan.png')) && path.extname(file).should.equal('.png') ) {
+      	done();
+      }      
+    })
+  })
+})
