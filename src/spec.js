ebd25715496384373f8162bfb58c90430a20f34f
yeraycaballero <yeraycaballero@gmail.com>
2013-04-07 22:15:20 +0100

    modify spec implementation

--- src/spec.js
+++ src/spec.js
@@ -1,15 +1,41 @@
-var _ = require('lodash');
+var _    = require('lodash'),
+    attr = require('./attributes'),
+    op   = require('./operators');
 
-var Spec = function(config) {
-  this.config = config;
-}
+var Spec = function(query) {
+  this.query = query;
+};
 
-Spec.prototype.satisfies = function(metadata) {
+Spec.prototype.satisfies = function(file, callback) {
+  var propertiesCount = Object.keys(this.query).length;
+  var index = 1;
   var result = true;
-  for (var key in this.config) {  
-  	result = result && _.isEqual(this.config[key], metadata[key]);
+  
+  for (var key in this.query) {
+    this.satisfiesProperty(file, key, function(res) {
+      result = result && res;
+
+      if (result && index++ == propertiesCount) {
+        callback.call(this, file);  
+      }
+    }) 
   }
-  return result;
+};
+
+Spec.prototype.satisfiesProperty = function(file, key, callback) {
+    var attributeHandler = attr[key];
+
+    if (attributeHandler == null || attributeHandler == undefined) throw new Error('Attribute: ' + key + ' not found.');
+
+    attributeHandler.apply(this, [file, function(err, res) {
+      if (err) return callback.call(this, false);
+
+      var value = this.query[key];
+      var context  = res;
+      var operator = (typeof value == 'function')? value : op.eq.call(context, value);
+      
+      callback.call(this, operator.call(context));
+    }]);  
 }
 
 exports.Spec = Spec;
--- test/spec.test.js
+++ test/spec.test.js
@@ -1,13 +1,46 @@
 
-var Spec = require('../src/spec').Spec,
-    should = require('should');
+var fs     = require('fs'),
+    should = require('should'),
+    Spec   = require('../src/spec').Spec,
+    op     = require('../src/operators');
+
 
 describe('Spec', function() {
 
-  it ('should pass a specification with a filename property', function() {
+  it ('should pass a specification with a filename property', function(done) {
+  	var filename = '../assets/jordan.png';
   	var spec     = new Spec({filename : 'jordan'});
-  	var metadata = {filename : 'jordan'};
-  	spec.satisfies(metadata).should.be.true;
-  })
+
+  	spec.satisfies(filename, function(res) {
+  		if (filename.should.equal(res)) done();
+  	});
+  });
+
+  it ('should pass a specification with a eq operator', function(done) {
+  	var filename = '../assets/jordan.png';
+  	var spec     = new Spec({filename : op.eq('jordan')});
+
+  	spec.satisfies(filename, function(res) {
+  		if (filename.should.equal(res)) done();
+  	});
+  });
+
+  it ('should pass a specification with a lt operator', function(done) {
+  	var filename = './assets/jordan.png';
+  	var spec     = new Spec({size : op.lt(20000)});
+
+  	spec.satisfies(filename, function(res) {		
+  		if (filename.should.equal(res)) done();
+  	});
+  });
+
+  it ('should pass a specification with a gt operator', function(done) {
+    var filename = './assets/jonshon.jpg';
+    var spec     = new Spec({size : op.gt(20000)});
+
+    spec.satisfies(filename, function(res) {   
+      if (filename.should.equal(res)) done();
+    });
+  });
 
 })
