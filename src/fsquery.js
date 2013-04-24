4fe99d993503f7b9f9cd36fb1e8f8ef97a085fc4
yeraycaballero <yeraycaballero@gmail.com>
2013-04-24 22:04:55 +0100

    comment registerAttributeHandler and getAttributeHandler

--- src/fsquery.js
+++ src/fsquery.js
@@ -8,16 +8,16 @@ var fsquery = {
   in : function(path) {
     var clazz = require('./query').Query;
     return new clazz(path);
-  },
-
-  registerAttributeHandler : function(attributeName, handler) {
-    this.attributes.push(attributeName);
-    this.handlers[attributeName] = handler;
-  },
-
-  getAttributeHandler : function(attributeName) {
-    return this.handlers[attributeName];
   }
+
+  // registerAttributeHandler : function(attributeName, handler) {
+  //   this.attributes.push(attributeName);
+  //   this.handlers[attributeName] = handler;
+  // },
+
+  // getAttributeHandler : function(attributeName) {
+  //   return this.handlers[attributeName];
+  // }
 
 };
 
